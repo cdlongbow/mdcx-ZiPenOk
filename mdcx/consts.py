6923d0e33dc81e4b8cff3a350e8d70074d1e606e
ZiPenOk <freetuchuang@gmail.com>
2026-07-06 07:37:42 +0800

    Bump version to 220260706

--- mdcx/consts.py
+++ mdcx/consts.py
@@ -2,7 +2,7 @@ import platform
 import sys
 from pathlib import Path
 
-LOCAL_VERSION = 220260518
+LOCAL_VERSION = 220260706
 
 GITHUB_REPO = "ZiPenOk/mdcx"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases"
